6ae7e4a63e79b002e2a677dc8feab8bab7bc9311
DVHKMA2107 <hoadamviet@gmail.com>
2022-04-26 17:48:55 +0700

    design app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,99 @@
-import "./App.css";
+import "./App.scss";
+import image1 from "./assets/image-1.jpg";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="app">
+      <nav className="navbar-app">Navbar</nav>
+      <nav className="navbar-board">Board Bar</nav>
+      <div className="board-colums">
+        <div className="column">
+          <header>BrainStroms</header>
+          <ul>
+            <li>
+              <img src={image1} alt="hoadamviet" />
+              Title: Dam Viet Hoa (Jerry)
+            </li>
+            <li>Add what you would like to work on below</li>
+            <li>Add what you would like to work on below</li>
+            <li>Add what you would like to work on below</li>
+            <li>Add what you would like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>BrainStroms</header>
+          <ul>
+            <li>
+              <img src={image1} alt="hoadamviet" />
+              Title: Dam Viet Hoa (Jerry)
+            </li>
+            <li>Add what you would like to work on below</li>
+            <li>Add what you would like to work on below</li>
+            <li>Add what you would like to work on below</li>
+            <li>Add what you would like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>BrainStroms</header>
+          <ul>
+            <li>
+              <img src={image1} alt="hoadamviet" />
+              Title: Dam Viet Hoa (Jerry)
+            </li>
+            <li>Add what you would like to work on below</li>
+            <li>Add what you would like to work on below</li>
+            <li>Add what you would like to work on below</li>
+            <li>Add what you would like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>BrainStroms</header>
+          <ul>
+            <li>
+              <img src={image1} alt="hoadamviet" />
+              Title: Dam Viet Hoa (Jerry)
+            </li>
+            <li>Add what you would like to work on below</li>
+            <li>Add what you would like to work on below</li>
+            <li>Add what you would like to work on below</li>
+            <li>Add what you would like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>BrainStroms</header>
+          <ul>
+            <li>
+              <img src={image1} alt="hoadamviet" />
+              Title: Dam Viet Hoa (Jerry)
+            </li>
+            <li>Add what you would like to work on below</li>
+            <li>Add what you would like to work on below</li>
+            <li>Add what you would like to work on below</li>
+            <li>Add what you would like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>BrainStroms</header>
+          <ul>
+            <li>
+              <img src={image1} alt="hoadamviet" />
+              Title: Dam Viet Hoa (Jerry)
+            </li>
+            <li>Add what you would like to work on below</li>
+            <li>Add what you would like to work on below</li>
+            <li>Add what you would like to work on below</li>
+            <li>Add what you would like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
