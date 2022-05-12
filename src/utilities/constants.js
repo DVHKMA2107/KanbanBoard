773d6b6da9eb2630e77fed7de05f2e501c411377
DVHKMA2107 <hoadamviet@gmail.com>
2022-05-12 23:38:10 +0700

    deploy app to firebase

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,3 +1,4 @@
-export const API_ROOT = 'http://localhost:2107'
+// export const API_ROOT = 'http://localhost:2107'
+export const API_ROOT = 'https://trello-app-server.herokuapp.com'
 export const MODAL_ACTION_CONFIRM = 'MODAL_ACTION_CONFIRM'
 export const MODAL_ACTION_CLOSE = 'MODAL_ACTION_CLOSE'
